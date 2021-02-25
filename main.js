93801f8f76384c7dfbd98285cc0b78f712947bfd
Jose Bocanegra <j.bocanegra@uniandes.edu.co>
2021-02-25 14:03:45 -0500

    First commit

--- main.js
+++ main.js
@@ -1,34 +1,21 @@
-/*const sourceName = document.getElementById("name");
-sourceName.addEventListener("input", (element) => {
-  document.getElementById("final-name").textContent = element.target.value;
-});
-
-const sourceCode = document.getElementById("code");
-sourceCode.addEventListener("input", (element) => {
-  document.getElementById("final-code").textContent = element.target.value;
-});
-
-const sourceEmail = document.getElementById("email");
-sourceEmail.addEventListener("input", (element) => {
-  document.getElementById("final-email").textContent = element.target.value;
-});*/
-
 const finalName = document.getElementById("final-name");
 const finalCode = document.getElementById("final-code");
 const finalEmail = document.getElementById("final-email");
 
-finalName.contentEditable = true;
-finalCode.contentEditable = true;
-finalEmail.contentEditable = true;
+const controls = [
+  { control: finalName, text: "Nombre" },
+  { control: finalCode, text: "Código" },
+  { control: finalEmail, text: "Correo electrónico" },
+];
 
-finalName.addEventListener("click", (element) => {
-  if (element.target.innerHTML.includes("[")) element.target.innerHTML = "";
-});
+controls.forEach((item) => {
+  item.control.contentEditable = true;
+  item.control.addEventListener("click", (element) => {
+    if (element.target.innerHTML.includes("[")) element.target.innerHTML = "";
+  });
 
-finalCode.addEventListener("click", (element) => {
-  if (element.target.innerHTML.includes("[")) element.target.innerHTML = "";
-});
-
-finalEmail.addEventListener("click", (element) => {
-  if (element.target.innerHTML.includes("[")) element.target.innerHTML = "";
+  item.control.addEventListener("focusout", (element) => {
+    if (element.target.innerHTML === "")
+      element.target.innerHTML = `${item.text} [Clic para editar]`;
+  });
 });
